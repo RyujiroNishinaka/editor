97492cb6513d127c2f12328bb235ebeb84d6bda5
RyujiroNishinaka <ryu410ryu@gmail.com>
2019-11-03 19:56:09 +0900

    node.jsからmongoが使えるようmongo.jsを修正

--- editor/mongo.js
+++ editor/mongo.js
@@ -4,27 +4,23 @@ var assert = require('assert');
 
 // Connection URL
 var url = 
-'mongodb://localhost:27017/MemoDB';
+'mongodb://localhost:27017';
 
 // Use connect method to connect to the Server
 MongoClient.connect(url, function(err, mongodb) {
     assert.equal(null, err);
-    console.log("Connected correctly to 
-server");
-    db = mongodb;
+    console.log("Connected correctly to server");
+    db = mongodb.db("MemoDB");
     if (err) throw err;
-    var obj = { name: "Company Inc", address: "Highway 37" };
-// ----------------------------------------------------------------------
-// TEST INSERT
-// ----------------------------------------------------------------------
-    db.collection("testCollection").insertOne(obj , function(err, res) {
+    var obj = { name: "Ichiro", username: "ichiro", password: "fuga" };
+    // ----------------------------------------------------------------------
+    // TEST INSERT
+    // ----------------------------------------------------------------------
+    db.collection("test1").insertOne(obj , function(err, res) {
       if (err) throw err;
-      db.close();
+      mongodb.close();
     });
 });
-
-db.createCollection('testCollection');
-
 
 var collection = function( name ) {
     return db.collection( name );
